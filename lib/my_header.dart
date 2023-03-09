bc31c8be270b00704895e41c61e4488304fb8ed8
dabuzida <dabuzida@gmail.com>
2023-03-09 18:04:27 +0900

    test

--- lib/my_header.dart
+++ lib/my_header.dart
@@ -11,12 +11,17 @@ class MyHeader extends StatefulWidget {
 
 class _MyHeaderState extends State<MyHeader> {
   String _selected = _languages[0];
+  String dropdownValue = 'Dog';
+  final double _headerFontSize = 14.0;
+  final FontWeight _fontWeight = FontWeight.w500;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         Container(
-          color: const Color(0xFF000000),
+          color: Color(0xffffffff),
+          // color: const Color(0xFF000000),
           // color: const Color(0xFFFFFFFF),
           width: double.infinity,
           child: Column(
@@ -24,38 +29,44 @@ class _MyHeaderState extends State<MyHeader> {
             children: <Widget>[
               const SizedBox(height: 100),
               // 37 8 6
-              Container(
-                // width: 56,
-                color: const Color(0xFFEBEBEB),
-                // DropdownButtonFormField
-                // DropdownButtonHideUnderline
-                // DropdownButton
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    value: _selected,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    elevation: 0,
-                    dropdownColor: const Color(0xFFFFFFFF),
-                    borderRadius: const BorderRadius.all(Radius.circular(3.0)),
-                    style: const TextStyle(color: Color(0xFF404040)),
-                    // underline: const SizedBox(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selected = newValue!;
-                      });
-                    },
-                    items: _languages.map<DropdownMenuItem<String>>(
-                      (String? value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(
-                            value ?? '',
-                          ),
-                        );
-                      },
-                    ).toList(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    // width: 56,
+                    color: const Color(0xFFEBEBEB),
+                    // DropdownButtonFormField
+                    // DropdownButtonHideUnderline
+                    // DropdownButton
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: _selected,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        elevation: 0,
+                        dropdownColor: const Color(0xFFFFFFFF),
+                        borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+                        style: TextStyle(
+                          color: const Color(0xFF404040),
+                          fontSize: _headerFontSize,
+                          fontWeight: _fontWeight,
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selected = newValue ?? '-';
+                          });
+                        },
+                        items: _languages.map<DropdownMenuItem<String>>(
+                          (String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
               const SizedBox(height: 100),
               Container(
